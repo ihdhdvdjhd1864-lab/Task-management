3ad5a6105fdd3d03f7f9d3d6518bccb09a1d1b5c
Karim Mohamed <ihdhdvdjhd1864@gmail.com>
2026-07-28 02:29:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 let Task = document.querySelector(".Task");
 let input = document.querySelector("input");
 let dilidAll = document.querySelector(".dilid");
-let ul = document.querySelector("ul");
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+let ul = document.querySelector("#ul");
+let count = document.querySelector("#count");
+let up = document.querySelector(".up");
 
-function saveTasks() {
-  let allTasks = [];
-
-  document.querySelectorAll("li").forEach((li) => {
-    let span = li.querySelector("span");
-
-    allTasks.push({
-      text: span.textContent,
-      completed: span.style.textDecoration === "line-through",
-    });
-  });
-
-  localStorage.setItem("tasks", JSON.stringify(allTasks));
+let dadaTscks = [];
+if (localStorage.getItem("Tascksll") != null) {
+  dadaTscks = JSON.parse(localStorage.getItem("Tascksll"));
+} else {
+  dadaTscks = [];
 }
 
 Task.addEventListener("click", function () {
-  if (input.value !== "") {
+  if (input.value.trim() === "") return;
+  let dadeTSWOsck = {
+    titel: input.value,
+    completed: false,
+  };
+  dadaTscks.push(dadeTSWOsck);
+  localStorage.setItem("Tascksll", JSON.stringify(dadaTscks));
+  input.value = "";
+  shawdade();
+  updateCount();
+  All();
+  lafture();
+});
+function shawdade() {
+  ul.innerHTML = "";
+  for (let i = 0; i < dadaTscks.length; i++) {
     let li = document.createElement("li");
+    li.classList.add("li");
     let span = document.createElement("span");
-    let div = document.createElement("div");
-    div.classList.add("divv");
     span.classList.add("span11");
     let tadal = document.createElement("button");
     tadal.classList.add("tadal");
@@ -32,59 +39,81 @@ Task.addEventListener("click", function () {
     mktamll.classList.add("mktamll");
     let DiliD = document.createElement("button");
     DiliD.classList.add("DiliD");
-
+    let div = document.createElement("div");
+    div.classList.add("div1100");
     li.appendChild(span);
     div.appendChild(DiliD);
     div.appendChild(tadal);
     div.appendChild(mktamll);
-    li.appendChild(div);
-    span.textContent = input.value;
     ul.appendChild(li);
-    saveTasks();
-
+    li.appendChild(div);
+    span.textContent = dadaTscks[i].titel;
+    if (dadaTscks[i].completed === true) {
+      span.style.cssText = `
+    text-decoration: line-through;
+    font-size: 14px;
+    color: rgb(144, 144, 144);
+  `;
+    }
     DiliD.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
     mktamll.innerHTML = `<i class="fa-solid fa-check"></i>`;
     tadal.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
     input.value = "";
-
     All();
     DiliD.addEventListener("click", function () {
       if (confirm("هل انت متاكد من حذف هذه المهمه؟")) {
         li.remove();
-        saveTasks();
-        All();
+        dadaTscks.splice(i, 1);
+        localStorage.setItem("Tascksll", JSON.stringify(dadaTscks));
+        shawdade();
+        updateCount();
+        lafture();
       }
     });
+
     tadal.addEventListener("click", function () {
       let newTask = prompt("اكتب المهمه الجديده", span.textContent);
       if (newTask !== null && newTask.trim() !== "") {
+        dadaTscks[i].titel = newTask;
         span.textContent = newTask;
-        saveTasks();
-        span.style.cssText = `
-font-size: 20px;
-transition: 0.5s;
-color: rgb(255, 255, 255);
-  `;
       }
+
+      localStorage.setItem("Tascksll", JSON.stringify(dadaTscks));
+      shawdade();
+      lafture();
     });
+
     mktamll.addEventListener("click", function () {
-      span.style.cssText = `
-text-decoration: line-through;
-font-size: 14px;
-transition: 0.5s;
-    color: white;
-  `;
-      saveTasks();
+      dadaTscks[i].completed = !dadaTscks[i].completed;
+      localStorage.setItem("Tascksll", JSON.stringify(dadaTscks));
+      if (dadaTscks[i].completed) {
+        span.style.cssText = `
+      text-decoration: line-through;
+      font-size: 14px;
+      color: rgb(144, 144, 144);
+      transition: 0.5s ease;
+    `;
+      } else {
+        span.style.cssText = `
+      text-decoration: none;
+      font-size: 20px;
+      color: rgb(255, 255, 255);
+      transition: 0.5s ease;
+    `;
+      }
+
+      shawdade();
+      lafture();
     });
-  } else {
-    alert("اكتب مهام الاول ؟");
   }
-});
+  lafture();
+}
 input.addEventListener("keydown", function (e) {
   if (e.key === "Enter") {
     Task.click();
   }
 });
+
 function All() {
   if (ul.children.length > 0) {
     dilidAll.style.cssText = `
@@ -96,88 +125,79 @@ function All() {
         `;
   }
 }
+
 dilidAll.addEventListener("click", function () {
   if (confirm("هل انت متاكد مسح جميع مهام؟")) {
     ul.innerHTML = "";
-    saveTasks();
+    localStorage.removeItem("Tascksll");
+    dadaTscks = [];
     input.value = "";
     All();
+    updateCount();
+    lafture();
   }
 });
-window.onload = function () {
-  tasks.forEach((task) => {
-    let li = document.createElement("li");
-    let span = document.createElement("span");
-    span.classList.add("span11");
 
-    let tadal = document.createElement("button");
-    tadal.classList.add("tadal");
+shawdade();
 
-    let mktamll = document.createElement("button");
-    mktamll.classList.add("mktamll");
+function updateCount() {
+  count.textContent = dadaTscks.length;
+}
+updateCount();
 
-    let DiliD = document.createElement("button");
-    DiliD.classList.add("DiliD");
-
-    span.textContent = task.text;
-
-    if (task.completed) {
-      span.style.cssText = `
-text-decoration: line-through;
-font-size: 14px;
-transition: 0.5s;
-color: white;
-`;
-    }
-
-    DiliD.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
-    mktamll.innerHTML = `<i class="fa-solid fa-check"></i>`;
-    tadal.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
-
-    let div = document.createElement("div");
-    div.classList.add("divv");
-
-    div.appendChild(DiliD);
-    div.appendChild(tadal);
-    div.appendChild(mktamll);
-
-    li.appendChild(span);
-    li.appendChild(div);
-
-    ul.appendChild(li);
-
-    DiliD.addEventListener("click", function () {
-      li.remove();
-      saveTasks();
-      All();
-    });
-
-    tadal.addEventListener("click", function () {
-      let newTask = prompt("اكتب المهمه الجديده", span.textContent);
-
-      if (newTask !== null && newTask.trim() !== "") {
-        span.textContent = newTask;
-        saveTasks();
-        span.style.cssText = `
-font-size: 20px;
-transition: 0.5s;
-font-weight: 700;
-color: rgb(255, 255, 255);
-`;
-
-      }
-    });
-
-    mktamll.addEventListener("click", function () {
-      span.style.cssText = `
-text-decoration: line-through;
-font-size: 14px;
-transition: 0.5s;
-color: white;
-`;
-      saveTasks();
-    });
+up.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
   });
+});
 
-  All();
-};
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 200) {
+    up.style.cssText = `
+    display: flex;
+    transition: 0.5s ease;
+  `;
+  } else {
+    up.style.cssText = `
+    display: none;
+    transition: 0.5s ease;
+  `;
+  }
+});
+
+function lafture() {
+  let la123 = document.querySelector("#la123");
+  let rema = document.querySelector("#rema");
+  let completedCount = dadaTscks.filter((task) => task.completed).length;
+  la123.textContent = completedCount;
+  rema.textContent = dadaTscks.length - completedCount;
+}
+
+lafture();
+
+function getDade() {
+  // انشاء  ساعه رقميه
+  let dadeTimr2 = document.querySelector(".dade-timr");
+
+  let dadeTaim = new Date();
+
+  let getFullYear = String(dadeTaim.getFullYear());
+  let getMonth = String(dadeTaim.getMonth() + 1).padStart(2, "0");
+  let getDate = String(dadeTaim.getDate()).padStart(2, "0");
+  dadeTimr2.innerHTML = `
+<i class="fa-solid fa-calendar-days"></i>
+<span>تاريخ اليوم :</span>
+<strong>${getDate} / ${getMonth} / ${getFullYear}</strong>
+`;
+  let dadeHour = document.querySelector(".dade-hour");
+  let getHours = String(dadeTaim.getHours()).padStart(2, "0");
+  let getMinutes = String(dadeTaim.getMinutes()).padStart(2, "0");
+  let getSeconds = String(dadeTaim.getSeconds()).padStart(2, "0");
+  dadeHour.innerHTML = `
+<i class="fa-solid fa-clock"></i>
+<span>الوقت الآن :</span>
+<strong>${getHours} : ${getMinutes} : ${getSeconds}</strong>
+`;
+}
+setInterval(getDade, 1000);
